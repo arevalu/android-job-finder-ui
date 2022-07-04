dd6db41e8159bca800d7054d88601900f0279c83
arevalo.luciana <arevalolucianadg@gmail.com>
2022-07-04 20:01:50 -0300

    chore: create layout

--- app/src/main/java/com/larevalo/jobfinder/MainActivity.kt
+++ app/src/main/java/com/larevalo/jobfinder/MainActivity.kt
@@ -2,10 +2,18 @@ package com.larevalo.jobfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.larevalo.jobfinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainJobList.layoutManager = LinearLayoutManager(this)
     }
 }
